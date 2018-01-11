0043ca5d7a62ec559e9bc7b19d0dda17da5f7696
Matt <matt.paauw@gmail.com>
2018-01-10 18:01:12 -0800

    Finished adding unit tests for all methods currently implemented within the Search class.

--- src/test/SearchTest.java
+++ src/test/SearchTest.java
@@ -11,48 +11,156 @@ import util.TestAssetEngine;
  */
 public class SearchTest {
 
+    private Search search;
     private TestAssetEngine engine;
 
     public SearchTest() {
+        this.search = new Search();
         this.engine = new TestAssetEngine();
     }
 
     @Test
     public void linearSearchValuePresentTest() {
-        Search search = new Search();
         int[] arr = this.engine.generateUnorderedIntegerArray();
         ItemIndex itemIndex = this.engine.chooseRandomIntegerIndexFromArray(arr);
         int expected = itemIndex.index;
-        int actual = search.linearSearch(arr, itemIndex.value);
+        int actual = this.search.linearSearch(arr, itemIndex.value);
         Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void linearSearchValueNotPresentTest() {
-        Search search = new Search();
         int[] arr = this.engine.generateUnorderedIntegerArray();
         int value = this.engine.generateRandomIntegerNotInArray(arr);
         int expected = -1;
-        int actual = search.linearSearch(arr, value);
+        int actual = this.search.linearSearch(arr, value);
         Assert.assertEquals(expected, actual);
     }
 
     @Test
-    public void linearSearchSingleValueTest() {
-        Search search = new Search();
+    public void linearSearchSingleValueArrayTest() {
         int[] arr = {6};
         int value = 6;
         int expected = 0;
-        int actual = search.linearSearch(arr, value);
+        int actual = this.search.linearSearch(arr, value);
         Assert.assertEquals(expected, actual);
     }
 
-    @Test public void linearSearchEmptyArrayTest() {
-        Search search = new Search();
+    @Test
+    public void linearSearchEmptyArrayTest() {
         int[] arr = {};
         int value = this.engine.generateRandomIntegerNotInArray(arr);
         int expected = -1;
-        int actual = search.linearSearch(arr, value);
+        int actual = this.search.linearSearch(arr, value);
         Assert.assertEquals(expected, actual);
     }
+
+    @Test
+    public void linearSearchRecursiveValuePresentTest() {
+        int[] arr = this.engine.generateUnorderedIntegerArray();
+        ItemIndex itemIndex = this.engine.chooseRandomIntegerIndexFromArray(arr);
+        int expected = itemIndex.index;
+        int actual = this.search.linearSearchRecursive(arr, itemIndex.value, 0);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void linearSearchRecursiveValueNotPresentTest() {
+        int[] arr = this.engine.generateUnorderedIntegerArray();
+        int value = this.engine.generateRandomIntegerNotInArray(arr);
+        int expected = -1;
+        int actual = this.search.linearSearchRecursive(arr, value, 0);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void linearSearchRecursiveSingleValueArrayTest() {
+        int[] arr = {6};
+        int value = 6;
+        int expected = 0;
+        int actual = this.search.linearSearchRecursive(arr, value, 0);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void linearSearchRecursiveEmptyArrayTest() {
+        int[] arr = {};
+        int value = this.engine.generateRandomIntegerNotInArray(arr);
+        int expected = -1;
+        int actual = this.search.linearSearchRecursive(arr, value, 0);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void binarySearchValuePresentTest() {
+        int[] arr = this.engine.generateOrderedIntegerArray();
+        ItemIndex itemIndex = this.engine.chooseRandomIntegerIndexFromArray(arr);
+        int expected = itemIndex.index;
+        int actual = this.search.binarySearch(arr, itemIndex.value);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void binarySearchValueNotPresentTest() {
+        int[] arr = this.engine.generateOrderedIntegerArray();
+        int value = this.engine.generateRandomIntegerNotInArray(arr);
+        int expected = -1;
+        int actual = this.search.binarySearch(arr, value);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void binarySearchSingleValueArrayTest() {
+        int[] arr = {5};
+        int value = 5;
+        int expected = 0;
+        int actual = this.search.binarySearch(arr, value);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void binarySearchEmptyArrayTest() {
+        int[] arr = {};
+        int value = this.engine.generateRandomIntegerNotInArray(arr);
+        int expected = -1;
+        int actual = this.search.binarySearch(arr, value);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void binarySearchRecursiveValuePresentTest() {
+        int[] arr = this.engine.generateOrderedIntegerArray();
+        ItemIndex itemIndex = this.engine.chooseRandomIntegerIndexFromArray(arr);
+        int expected = itemIndex.index;
+        int actual = this.search.binarySearchRecursive(arr, itemIndex.value, 0, arr.length - 1);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void binarySearchRecursiveValueNotPresentTest() {
+        int[] arr = this.engine.generateOrderedIntegerArray();
+        int value = this.engine.generateRandomIntegerNotInArray(arr);
+        int expected = -1;
+        int actual = this.search.binarySearchRecursive(arr, value, 0, arr.length - 1);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void binarySearchRecursiveSingleValueArrayTest() {
+        int[] arr = {5};
+        int value = 5;
+        int expected = 0;
+        int actual = this.search.binarySearchRecursive(arr, value, 0, arr.length - 1);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void binarySearchRecursiveEmptyArrayTest() {
+        int[] arr = {};
+        int value = this.engine.generateRandomIntegerNotInArray(arr);
+        int expected = -1;
+        int actual = this.search.binarySearchRecursive(arr, value, 0, arr.length - 1);
+        Assert.assertEquals(expected, actual);
+    }
+
 }
