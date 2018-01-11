afcc3001f8fc52433ada5a4b05d14ae222b7bbea
Matt <matt.paauw@gmail.com>
2018-01-10 17:14:06 -0800

    Added small ItemIndex container class.

---
+++ src/util/ItemIndex.java
@@ -0,0 +1,16 @@
+package util;
+
+/**
+ * Container implementation used for returning value-index pairs.
+ */
+public class ItemIndex {
+    int value;
+    int index;
+
+    public ItemIndex() {}
+
+    public ItemIndex(int value, int index) {
+        this.value = value;
+        this.index = index;
+    }
+}
