52b6adce0e15c2fa89b53bdab729f4ff25318d5f
Matt <matt.paauw@gmail.com>
2018-01-10 17:39:43 -0800

    Debugged test asset engine.

--- src/util/TestAssetEngine.java
+++ src/util/TestAssetEngine.java
@@ -9,16 +9,26 @@ import java.util.Random;
 public class TestAssetEngine {
 
     private Random rand = new Random();
-    private final int UPPER_BOUND = Integer.MAX_VALUE;
+    private final int UPPER_BOUND_VALUE = Integer.MAX_VALUE;
+    private final int UPPER_BOUND_SIZE = 1000000;
 
     public TestAssetEngine() {}
 
     /**
      * Generates a random signed integer.
-     * @return the generated integer.
+     * @return the randomly generated integer.
      */
     public int generateRandomInteger() {
-        return generateSign() ? this.rand.nextInt(UPPER_BOUND) : -1 * this.rand.nextInt(UPPER_BOUND);
+        return generateSign() ? this.rand.nextInt(UPPER_BOUND_VALUE) : -1 * this.rand.nextInt(UPPER_BOUND_VALUE);
+    }
+
+    /**
+     * Generates a random signed integer given a specified upper bound.
+     * @param bound the upper bound to use during integer generation.
+     * @return the randomly generated integer.
+     */
+    public int generateRandomIntegerWithBound(int bound) {
+        return generateSign() ? this.rand.nextInt(bound) : -1 * this.rand.nextInt(bound);
     }
 
     /**
@@ -26,7 +36,7 @@ public class TestAssetEngine {
      * @return the generated array.
      */
     public int[] generateUnorderedIntegerArray() {
-        int arraySize = this.rand.nextInt(UPPER_BOUND);
+        int arraySize = this.rand.nextInt(UPPER_BOUND_SIZE);
         int[] unorderedArr = new int[arraySize];
         for(int i = 0; i < unorderedArr.length; i++) {
             unorderedArr[i] = generateRandomInteger();
@@ -39,13 +49,36 @@ public class TestAssetEngine {
      * @return the generated array.
      */
     public int[] generateOrderedIntegerArray() {
-        int arraySize = this.rand.nextInt(UPPER_BOUND);
+        int arraySize = this.rand.nextInt(UPPER_BOUND_SIZE);
         int[] orderedArr = new int[arraySize];
         for(int i = 0; i < orderedArr.length; i++) {
             orderedArr[i] = generateRandomInteger();
         }
         Arrays.sort(orderedArr);
         return orderedArr;
+    }
+
+    /**
+     * Randomly chooses an integer-index pair from a given array.
+     * @param arr the array to choose from.
+     * @return the randomly chosen ItemIndex value.
+     */
+    public ItemIndex chooseRandomIntegerIndexFromArray(int[] arr) {
+        int index = this.rand.nextInt(arr.length);
+        return new ItemIndex(arr[index], index);
+    }
+
+    /**
+     * Randomly generates an integer that does not exist in a given array.
+     * @param arr the array to use for reference.
+     * @return the randomly generated integer.
+     */
+    public int generateRandomIntegerNotInArray(int[] arr) {
+        int value = generateRandomIntegerWithBound(UPPER_BOUND_VALUE);
+        while(Arrays.asList(arr).contains(value)) {
+            value = generateRandomIntegerWithBound(UPPER_BOUND_VALUE);
+        }
+        return value;
     }
 
     /**
