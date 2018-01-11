543995e6cba7600f4cd356085b5b4a5f3d0132fe
Matt <matt.paauw@gmail.com>
2018-01-10 17:40:20 -0800

    Equipped SearchTest unit test class with new TestAssetEngine utility.

--- src/test/SearchTest.java
+++ src/test/SearchTest.java
@@ -3,27 +3,35 @@ package test;
 import org.junit.Test;
 import org.junit.Assert;
 import search.Search;
+import util.ItemIndex;
+import util.TestAssetEngine;
 
 /**
  * Contains unit tests for methods contains within the Search class.
  */
 public class SearchTest {
 
+    private TestAssetEngine engine;
+
+    public SearchTest() {
+        this.engine = new TestAssetEngine();
+    }
+
     @Test
     public void linearSearchValuePresentTest() {
         Search search = new Search();
-        int[] arr = {6, 11, 50, 0, 5, 1000};
-        int value = 5;
-        int expected = 4;
-        int actual = search.linearSearch(arr, value);
+        int[] arr = this.engine.generateUnorderedIntegerArray();
+        ItemIndex itemIndex = this.engine.chooseRandomIntegerIndexFromArray(arr);
+        int expected = itemIndex.index;
+        int actual = search.linearSearch(arr, itemIndex.value);
         Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void linearSearchValueNotPresentTest() {
         Search search = new Search();
-        int[] arr = {6, 11, 50, 0, 5, 1000};
-        int value = 27;
+        int[] arr = this.engine.generateUnorderedIntegerArray();
+        int value = this.engine.generateRandomIntegerNotInArray(arr);
         int expected = -1;
         int actual = search.linearSearch(arr, value);
         Assert.assertEquals(expected, actual);
@@ -42,7 +50,7 @@ public class SearchTest {
     @Test public void linearSearchEmptyArrayTest() {
         Search search = new Search();
         int[] arr = {};
-        int value = 99;
+        int value = this.engine.generateRandomIntegerNotInArray(arr);
         int expected = -1;
         int actual = search.linearSearch(arr, value);
         Assert.assertEquals(expected, actual);
